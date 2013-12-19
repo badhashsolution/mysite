cda7a818999742b015e1e23bd7f80a0b0c41ca45
badhashsolution <badhashsolution@gmail.com>
2013-12-18 21:27:50 -0800

    You're looking at poll 34

--- polls/views.py
+++ polls/views.py
@@ -5,4 +5,12 @@ from django.http import HttpResponse
 def index(request):
 	return HttpResponse("Hello, world. You're at the poll index.")
 	
+def detail(request, poll_id):
+	return HttpResponse("You're looking at poll %s." % poll_id)
+	
+def results(request, poll_id):
+	return HttpResponse("You're looking at the results of poll %s." % poll_id)
+	
+def vote(request, poll_id):
+	return HttpREsponse("You're votina on poll %s." % poll_id)
 	

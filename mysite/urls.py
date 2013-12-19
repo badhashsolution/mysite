6fb7361150ddb630b77b5a919ad4f0e890d62a5d
badhashsolution <badhashsolution@gmail.com>
2013-12-18 21:50:52 -0800

    Revert "possibly bad changes"
    This reverts commit 18755794dba45f3188558f4d4c879fe1b9265230.

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-	url(r'^polls/', include('polls.urls')), # WGG 12/18/2013 wire an index view into URLconf
-   url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^admin/', include(admin.site.urls)),
 )
--- polls/views.py
+++ polls/views.py
@@ -3,20 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	latest_poll_list = Poll.objects.order_by('-pub_date') [:5]
-	template = loader.get_template('polls/index.html')
-	context = RequestContext(request, {
-		latest_poll_list': latest_poll_list,
-	})
+	return HttpResponse("Hello, world. You're at the pol index.")
 	
-	return HttpResponse(template.render(context))
-	
-def detail(request, poll_id):
-	return HttpResponse("You're looking at poll %s." % poll_id)
-	
-def results(request, poll_id):
-	return HttpResponse("You're looking at the results of poll %s." % poll_id)
-	
-def vote(request, poll_id):
-	return HttpREsponse("You're votina on poll %s." % poll_id)
 	

26b3134cfd0eaf33d4ffdc1e98b8d431b49b57d9
badhashsolution <badhashsolution@gmail.com>
2013-12-18 21:17:47 -0800

    added polls view

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello, world. You're at the pol index.")
+	return HttpResponse("Hello, world. You're at the poll index.")
 	
 	

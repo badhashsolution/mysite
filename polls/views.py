d74da669230cbfc0f272afbe4ae1da4e4253f9b5
badhashsolution <badhashsolution@gmail.com>
2013-12-18 22:03:34 -0800

    Got it working again
    this version is working

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+
+from polls.models import Poll
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello, world. You're at the poll index.")
+	latest_poll_list = Poll.objects.order_by('-pub_date') [:5]
+	output = ', '.join([p.question for p in latest_poll_list])	
+	return HttpResponse(output)
 	
 def detail(request, poll_id):
 	return HttpResponse("You're looking at poll %s." % poll_id)
@@ -12,5 +15,5 @@ def results(request, poll_id):
 	return HttpResponse("You're looking at the results of poll %s." % poll_id)
 	
 def vote(request, poll_id):
-	return HttpREsponse("You're votina on poll %s." % poll_id)
+	return HttpREsponse("You're voting on poll %s." % poll_id)
 	
